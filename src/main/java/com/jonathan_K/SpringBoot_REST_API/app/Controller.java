0c03d7034b1165b8f4345ca740218259561a0821
unknown <kyeyunejonathan001@gmail.com>
2025-02-10 23:05:15 +0300

    addes 2 methods to test @PathVariable and @RequestParam using endpoints in postman

--- src/main/java/com/jonathan_K/SpringBoot_REST_API/app/Controller.java
+++ src/main/java/com/jonathan_K/SpringBoot_REST_API/app/Controller.java
@@ -4,9 +4,11 @@
 package com.jonathan_K.SpringBoot_REST_API.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/api")
 public class Controller {
 
-    @GetMapping("/hello")
+   @GetMapping("/hello")
     public String sayHello() {
 	return "Hi Jonathan K";
     }
@@ -38,7 +40,32 @@ public class Controller {
 	
 	return "Hi Jonathan K , you posted this record order: "+order.toString() ;
   }
+   
+  
+    // working with parameters
+  //http://localhost:8080//rest/api/hello/{value}
+  //@PathVariable extracts values from url path
+  //used when value you is part of URI
+  @GetMapping("/hello/{user-name}")
+  public String pathVar(@PathVariable("user-name") String userName) {
+	return "My value = "+userName;
+  }
+  
+  // working with parameters request param
+  //http://localhost:8080//rest/api/hello?param_name=paramValue&param_value2=value2
+ //extracts query params from url, context info for request using ? and & symbols
+  //used when value is passed as query parameter
+  
+  @GetMapping("/hello")
+  public String pathVar(
+	  @RequestParam("user-name") String userName,
+	  @RequestParam("user-lastname") String userlastName
+	  
+	  ) {
+	return "My usernames are = "+userName+ "  "+userlastName;
+  }
   
   
-    
+  
+  
 }
